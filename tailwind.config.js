4798979596855c8f0dd36ae25dc65eb7e0862598
akadariya <akadariya@myseneca.ca>
2025-07-12 14:05:59 -0400

    Final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/**/*.ejs'],
+  content: [
+    "./views/**/*.ejs",    // scan your EJS templates
+    "./public/**/*.js"     // (if you ever generate classes in JS)
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+};
